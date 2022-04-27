1fecd01c9edcdb35ee09da60fc9cf7f904fe36d6
adcastroasticom <98745651+adcastroasticom@users.noreply.github.com>
2022-04-27 15:13:55 +0800

    Included validation in forms

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,53 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../image/blcis-logo.png";
-
 import '../css/home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 const Home = () => { 
+
+  const initialValues = { studentID: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+     e.preventDefault();
+    const errors = validate(formValues);
+    setFormErrors(errors);
+    if(Object.keys(errors).length === 0) {
+      e.target.reset();
+      window.location.href = "http://localhost:3000/Welcome";
+    }
+
+  };
+
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values.studentID) {
+      errors.studentID = "Student ID is required";
+    } else if ((values.studentID.length !== 10) || isNaN(values.studentID)) {
+      errors.studentID = "Student ID must be a 10 digit number";
+    } 
+
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password !== "password") {
+      errors.password = "Invalid password";
+    } 
+
+    if(Object.keys(errors).length === 0) {
+
+    }
+    return errors;
+  };
     return (
       <>
         <div className="home-container">
@@ -19,23 +61,19 @@ const Home = () => {
             <p> Sign in using your 10-digit student number. </p>
           </div>
 
-          <form className="login-form">
+          <form className="login-form" onSubmit={handleSubmit} >
             <label className="student-num"> Student Number </label> <br />
-            <input className="sn-input" type="text" placeholder="10-Digit Student Number" maxlength="10" required/>
-
-
-            <br /><br />
+            <input className="sn-input" type="text" name="studentID" placeholder="10-Digit Student Number" value={formValues.studentID} onChange={handleChange} />
+            <p className="text-danger">{formErrors.studentID}</p>
 
             <label className="pass-txt"> Password </label><br />
-            <input className="pw-input" type="password" placeholder="Password" required/>
+            <input className="pw-input" type="password" placeholder="Password" name="password" value={formValues.password} onChange={handleChange}/>
+            <p className="text-danger"> {formErrors.password} </p>
 
-            <br /><br />
-
-            <Link to="/Welcome"> <input type="submit" className="btn btn-primary" value="Login" id="login-btn" /> </Link>
+            <input type="submit" className="btn btn-primary" value="Login" id="login-btn" />
           </form>
 
           <p id="new-user"> New user? Please sign up <strong><Link to = "/Registration" id="link">here</Link></strong>. </p>
-
         </div>
       </>
     );
--- src/component/Registration.js
+++ src/component/Registration.js
@@ -6,13 +6,71 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '../css/registration.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
 
-const  handleFormSubmit = (e) => {
-  e.preventDefault();
-  e.target.reset();
-  toast.success("Successful Registration", {autoClose: 1000,hideProgressBar: true,})
-}
 const Registration = () => {
+
+const initialValues = { gName : "", lName:"", studentID: "", college: "", program: "", yearLevel: "", password: "" , cPassword: "" };
+const [formValues, setFormValues] = useState(initialValues);
+const [formErrors, setFormErrors] = useState({});
+
+const handleChange = (e) => {
+  const { name, value } = e.target;
+  setFormValues({ ...formValues, [name]: value });
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const errors = validate(formValues);
+  setFormErrors(errors);
+  if(Object.keys(errors).length === 0) {
+    e.target.reset();
+    toast.success("Successful Registration", {autoClose: 1000,hideProgressBar: true,})
+  }
+};
+
+
+const validate = (values) => {
+  const errors = {};
+  const passwordPattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-.]).{8,}$/;
+  if(!values.gName) {
+    errors.gName = "Given name is required";
+  }
+
+  if(!values.lName) {
+    errors.lName = "Last name is required";
+  }
+  if(!values.college) {
+    errors.college = "College is required";
+  }
+  if(!values.program) {
+    errors.program = "Program is required";
+  }
+  if(!values.yearLevel) {
+    errors.yearLevel = "Year Level is required";
+  }
+
+  if (!values.studentID) {
+    errors.studentID = "Student ID is required";
+  } else if ((values.studentID.length !== 10) || isNaN(values.studentID)) {
+    errors.studentID = "Student ID must be a 10 digit number";
+  } 
+  
+  if (!values.password) {
+    errors.password = "Password is required";
+  } else if (!passwordPattern.test(values.password)) {
+    errors.password = "Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters";
+  } 
+
+  if (!values.cPassword) {
+    errors.cPassword = "Confirm Password is required";
+  } else if (values.cPassword !== values.password ) {
+    errors.password = "Password does not match";
+  } 
+
+  return errors;
+};
+
   
     return (
       <>
@@ -28,12 +86,13 @@ const Registration = () => {
           <p> Create an account. </p>
         </div>
 
-        <form className="signup-form"  onSubmit={ handleFormSubmit }>
+        <form className="signup-form"  onSubmit={ handleSubmit }>
 
           <div className="row">
             <div className="col-lg-4">
               <label> Given Name </label><br/>
-              <input id="giv-name" className="form-control" type="text" placeholder="Given Name" required />
+              <input id="giv-name" className="form-control" type="text" placeholder="Given Name"  name="gName"  value={formValues.gName}  onChange={handleChange} />
+              <p className="text-danger">{formErrors.gName}</p>
             </div>
 
             <div className="col-lg-4">
@@ -43,50 +102,57 @@ const Registration = () => {
 
             <div className="col-lg-4">
               <label> Last Name </label><br/>
-              <input id="last-name" type="text" className="form-control" placeholder="Last Name" required />            
+              <input id="last-name" type="text" className="form-control" placeholder="Last Name" name="lName"  value={formValues.lName}  onChange={handleChange}  />            
+              <p className="text-danger">{formErrors.lName}</p>
             </div>
           </div>
 
           <div className="row">
             <div className="col-lg-12">
               <label> Student Number </label><br/>
-              <input id="student-no" className="form-control" type="text" placeholder="10-Digit Student Number" maxlength="10" required/>
+              <input id="student-no" className="form-control" type="text" placeholder="10-Digit Student Number" maxlength="10" name="studentID"  value={formValues.studentID}  onChange={handleChange} />
+              <p className="text-danger">{formErrors.studentID}</p>
             </div>
           </div>
 
           <div className="row">
             <div className="col-lg-4">
               <label> College </label><br/>
-              <input id="college" className="form-control" type="text" placeholder="College" required />
+              <input id="college" className="form-control" type="text" placeholder="College"  name="college"  value={formValues.college}  onChange={handleChange} />
+              <p className="text-danger">{formErrors.college}</p>
             </div>
 
             <div className="col-lg-4">
               <label> Program Enrolled </label><br/>
-              <input id="program" className="form-control" type="text" placeholder="Program Enrolled" required />            
+              <input id="program" className="form-control" type="text" placeholder="Program Enrolled"  name="program"  value={formValues.program}  onChange={handleChange} />            
+              <p className="text-danger">{formErrors.program}</p>
             </div>
 
             <div className="col-lg-4">
               <label id="yr-lvl"> Year Level </label><br/>
-              <select required className="form-select">
+              <select className="form-select"  name="yearLevel"  value={formValues.yearLevel}  onChange={handleChange}>
                 <option disabled selected> Select One </option>
                 <option> 1 </option>
                 <option> 2 </option>
                 <option> 3 </option>
                 <option> 4 </option>
                 <option> 5 </option>
-              </select>            
+              </select>
+              <p className="text-danger">{formErrors.yearLevel}</p>            
             </div>
           </div>
 
           <div className="row">
             <div className="col-lg-6">
               <label id="pass"> Password </label><br/>
-              <input type="password" className="form-control" placeholder="Password" required/>
+              <input type="password" className="form-control" placeholder="Password"  name="password"  value={formValues.password}  onChange={handleChange}/>
+              <p className="text-danger">{formErrors.password}</p>            
             </div>
 
             <div className="col-lg-6">
               <label id="confirm-pass"> Confirm Password </label><br/>
-              <input type="password" className="form-control" placeholder="Confirm Password" required/>
+              <input type="password" className="form-control" placeholder="Confirm Password"  name="cPassword"  value={formValues.cPassword}  onChange={handleChange}/>
+              <p className="text-danger">{formErrors.cPassword}</p>            
             </div>
           </div>
 
